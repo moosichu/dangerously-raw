164ee6e52de93b9d5495446f5ee0ce92e44ff45b
Tom Maenan Read Cutting <readcuttingt@gmail.com>
2018-03-01 20:04:55 +0000

    Remove unneeded todo

--- solver.py
+++ solver.py
@@ -18,7 +18,6 @@ def solve(list_of_cars, list_of_rides, sim_steps):
         )
 
         for car_id, ride_id in assignments.items():
-            # TODO: Remove cars from available cars list
             # TODO: add cars to cars in flight
             result[car_id].append(ride_id)
             pass
