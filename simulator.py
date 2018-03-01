34c560e772e89e0cffa801fed1f7158d85775da9
Jared Khan <jaredkhan@me.com>
2018-03-01 20:37:10 +0000

    Fix simulator

--- simulator.py
+++ simulator.py
@@ -1,24 +1,35 @@
 from car import Car
 
-#Will take a list of cars in flight of the form (car_id, current_position, destination_position)
-#Will make one step in time, move the cars along and return a tuple with the next cars in flight and the cars which are now available
-def move_cars(cars_in_flight): 
-  return_cars_in_flight = []
-  return_cars_free = []
-  for car in cars_in_flight:
-    new_car = Car()
-    new_car.destination = car.destination
-    new_car.id = car.id
-    if car.position[0] < car.destination[0]: #move right on x axis
-      new_car.position = (car.position[0] + 1, car.position[1])
-    elif car.position[0] > car.destination[0]: #move left on x axis
-      new_car.position = (car.position[0] - 1, car.position[1])
-    elif car.position[1] < car.destination[1]: #move up on y axis
-      new_car.position = (car.position[0], car.position[1] + 1)
-    elif car.position[1] > car.destination[1]: #move down on y axis
-      new_car.position = (car.position[0], car.position[1] - 1)
-    if new_car.position == new_car.destination: #car has arrived
-      return_cars_free.append(new_car)
+# Take a list of cars in flight of the form (car, ride, has_picked_up)
+# where has_picked_up indicates whether we have picked up the person from the start 
+# return list of cars in flight in t + 1 and the list of newly available cars
+
+def perform_time_step(t, cars_in_flight):
+  newly_available_cars = []
+  updated_cars_in_flight = []
+
+  for (car, ride, has_picked_up) in cars_in_flight:
+    # change pick up status if possible
+    if car.position == (ride["start_col"], ride["start_row"]) and t >= ride["earliest_start"]:
+      has_picked_up = True
+
+    # Figure out where we're heading next
+    next_destination = (ride["start_col"], ride["start_row"]) if has_picked_up else (ride["end_col"], ride["end_row"])
+
+    # make a move towards thing
+    if car.position[0] < next_destination[0]: #move right on x axis
+      car.position = (car.position[0] + 1, car.position[1])
+    elif car.position[0] > next_destination[0]: #move left on x axis
+      car.position = (car.position[0] - 1, car.position[1])
+    elif car.position[1] < next_destination[1]: #move up on y axis
+      car.position = (car.position[0], car.position[1] + 1)
+    elif car.position[1] > next_destination[1]: #move down on y axis
+      car.position = (car.position[0], car.position[1] - 1)
+
+    # If we are at the end of the ride, free up the car
+    if car.position == new_car.destination and has_picked_up: #car has arrived
+      newly_available_cars.append(car)
     else: #car is still in flight
-      return_cars_in_flight.append(new_car)
-  return(return_cars_in_flight, return_cars_free)
+      updated_cars_in_flight.append((car, ride, has_picked_up))
+
+  return (updated_cars_in_flight, newly_available_cars)
