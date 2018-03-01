99e184a65d3c10657547e27cae3b60b4fa897acd
Tom Maenan Read Cutting <readcuttingt@gmail.com>
2018-03-01 19:22:26 +0000

    Write complete pipeline including stubs

--- car.py
+++ car.py
@@ -6,3 +6,9 @@ class Car:
       return str(self)
     def __str__(self):
         return "id:" + str(self.id) + "; position: " + str(self.position) + "; destination: " + str(self.destination) + ";\n"
+
+
+def get_car_list(num_cars):
+  result = []
+  for car_num in range(0, num_cars):
+    result.append(Car())
---
+++ solver.py
@@ -0,0 +1,6 @@
+def solve(list_of_cars, list_of_rides, sim_steps):
+    # Currently a stub
+    return {
+        0: [0, 2, 6],   # car 0 does rides 0, 2 and 6
+        1: [1, 3, 4, 5] # car 1 does rides 1, 3, 4 and 5
+    }
